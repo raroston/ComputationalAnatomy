e79feeed382a26a6bcb703d7b40241d4f945eb3c
raroston <raroston@gmail.com>
2023-02-08 14:35:59 -0800

    Added option to specify transforms to omit from PCA

--- 2_PCA_multi.R
+++ 2_PCA_multi.R
@@ -7,15 +7,25 @@
 
 runID <- # set runID
 wd <- # set working directory
+omit <-  # pattern(s) used to select specific transforms to omit from PCA
 
 transform.type <- c("Total", "Syn1")
 masks <- c("head", "limbs", "torso", "wholebody")
 
 setwd(wd)
 
-runPCA <- function(wd = getwd(), transform.type, mask, runID){
+runPCA <- function(wd = getwd(), transform.type, mask, runID, omit){
   
   library(ANTsR)
+  
+  createtransformlist <- function(wd, folder, omit){
+    transformlist <- paste0(wd, folder, dir(paste0(wd, folder)))
+    
+    for (i in 1:length(omit)){
+      transformlist <- transformlist[-grep(patt = omit[i], transformlist)]
+    }
+    return(transformlist)
+  }
   
   doPCA <- function(dir.PCAresults, transformlist, pca_mask){
     
@@ -44,12 +54,14 @@ runPCA <- function(wd = getwd(), transform.type, mask, runID){
       pca_mask <- resampleImage(antsImageRead(maskfile), c(0.054,0.054,0.054), interpType = 1)
       
       if (transform.type == "Total"){
-        transformlist <- paste0(wd, "/TotalTransforms/", dir(paste0(wd, "/TotalTransforms/")))
+        folder <- "/TotalTransforms/"
+        transformlist = createtransformlist(wd = wd, folder = folder, omit = omit)
         dir.create(paste0(wd, "/PCA/", runID))
         dir.PCAresults <- paste0(wd, "/PCA/", runID, "/", transform.type, "-", mask)
         doPCA(dir.PCAresults = dir.PCAresults, transformlist = transformlist, pca_mask = pca_mask)
       } else if (transform.type == "Syn1") {
-        transformlist <- paste0(wd, "/Transforms/", dir(paste0(wd, "/Transforms/"), pattern = "_Warp"))
+        folder <- "/Transforms/"
+        transformlist = createtransformlist(wd = wd, folder = folder, omit = omit)
         dir.PCAresults <- paste0(wd, "/PCA/", runID, "/", transform.type, "-", mask)
         doPCA(dir.PCAresults = dir.PCAresults, transformlist = transformlist, pca_mask = pca_mask)
       } else { 
